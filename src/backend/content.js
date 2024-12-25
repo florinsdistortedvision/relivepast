228f6ffccad1c91933795fb78029a8ad90afdbe1
★ florin <97384306+florinsdistortedvision@users.noreply.github.com>
2024-12-25 09:48:17 +0200

    Update content.js

--- src/backend/content.js
+++ src/backend/content.js
@@ -7,7 +7,7 @@
 import content from "./content.json" assert { type: "json" };
 
 export function getContent() {
-  let day = new Date().getDay();
+  let day = new Date().getDate();
 
   let currentMonth = new Date()
     .toLocaleString("en-US", { month: "long" })
@@ -15,12 +15,12 @@ export function getContent() {
 
   let contentMonth = Object.keys(content);
 
-  if (day !== "25" && contentMonth.includes(currentMonth)) {
+  if (day !== 25 && contentMonth.includes(currentMonth)) {
     let month = content[currentMonth];
 
     return month;
   }
-  if (day === "25" && currentMonth === "december") {
+  if (day === 25 && currentMonth === "december") {
     let month = content["christmas"];
 
     return month;
